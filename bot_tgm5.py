d50926468593d6322b23a9dfc83fd3682d60afa7
Ilya Hlotau <iliaglo93@gmail.com>
2022-07-11 21:00:41 +0300

    add database sqllite3

--- bot_tgm5.py
+++ bot_tgm5.py
@@ -1,8 +1,10 @@
 from aiogram.utils import executor
 from create_bot import dp
+from database import sqlite_db
 
 async def on_startup(_):
 	print('Бот вышел в онлайн')
+	sqlite_db.sql_start()
 from handlers import client,admin,other
 client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
--- handlers/admin.py
+++ handlers/admin.py
@@ -4,7 +4,8 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
 from create_bot import dp,bot
 from aiogram.dispatcher.filters import Text
-
+from database import sqlite_db
+from keyboards import admin_kb
 ID = None
 
 
@@ -22,7 +23,7 @@ class FSMAdmin(StatesGroup):
 async def make_changes_command(message: types.Message):
     global ID
     ID = message.from_user.id
-    await bot.send_message(message.from_user.id, 'Что хозяин надо???')
+    await bot.send_message(message.from_user.id,'Что хозяин надо???', reply_markup=admin_kb.button_case_admin)
     await message.delete()
 
 
@@ -82,8 +83,7 @@ async def load_price(message: types.Message, state: FSMContext):
     if message.from_user.id == ID:
         async with state.proxy() as data:
             data['price'] = float(message.text)
-        async with state.proxy() as data:
-            await message.reply(str(data))
+        await sqlite_db.sql_add_command(state)
         await state.finish()  # команда завершает машино-состояние и все данные что мы получили выши нужно обработать
 
 
